86e2f04dbafd58b67bc14b3b82d7df5ee05d608f
Adil Altaf <khanadilaltaf@gmail.com>
2023-05-13 12:52:15 -0400

    Components

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,16 @@
-import Avatar from "./components/Avatar";
-import Gallery from "./components/Gallery";
-// import { Profile } from "./components/Gallery";
+import List from "./components/List";
+import PackingList from "./components/PackingList";
 import Profile from "./components/Profile";
-import ToDo from "./components/ToDo";
-import TodoList from "./components/ToDoList";
 
 export default function Home() {
   return (
     <div>
-      <Profile
+      <List />
+      {/* <PackingList /> */}
+      {/* <Profile
         person={{ name: "Lin Lanying", imageId: "1bX5QH6" }}
         size={200}
-      />
+      /> */}
     </div>
   );
 }
