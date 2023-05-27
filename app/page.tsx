ee40d50fa46fdeb7dc1bd078a06e3b2072efa141
Adil Altaf <khanadilaltaf@gmail.com>
2023-05-27 12:44:58 -0400

    useState

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
+import Form from "./components/Form";
 import SculptureGallery from "./components/SculptureGallery";
 
 export default function Home() {
   return (
     <div>
-      <SculptureGallery />
+      <Form />
     </div>
   );
 }
